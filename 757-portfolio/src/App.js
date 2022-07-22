952102d5e35cb590c70a1033b757ff00a270eac9
Giossaurus <gihhskt@gmail.com>
2022-07-22 12:46:21 -0300

    react project start

---
+++ 757-portfolio/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      title: '757 film',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'Sobre', path: '/sobre' },
+        { title: 'Contato', path: '/contato' }
+      ],
+      home: {
+        title: 'Texto qualquer 1',
+        subtitle: 'Texto menor',
+        text: 'texto teste'
+      },
+      sobre: {
+        title: 'Sobre nós',
+      },
+      contato: {
+        title: 'Fale com a gente',
+      }
+    }
+  }
+
+  render(){
+    return(
+      <div>Hello world</div>
+    );
+  }
+}
+
+export default App;
